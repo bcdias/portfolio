343e2f4293463c33f481795a21cf7931b69899aa
Bruno Dias <dias.desenvolvedor@gmail.com>
2022-05-17 13:38:50 -0300

    Animando textos da seção boas vindas

--- .portfolio/assets/js/main.js
+++ .portfolio/assets/js/main.js
@@ -1,17 +1,11 @@
 import abreFechaMenu from "./modulos/menuLateral.js";
 
-const menu = document.querySelector('[data-tipo="menu"]')
-const menuIcone = document.querySelector('[data-tipo="iconeMenu"]')
-const listaLinkMenu = document.querySelectorAll('[data-tipo="linkMenu"]')
-
-console.log(listaLinkMenu);
-
-menuIcone.addEventListener('click', () =>{
-    abreFechaMenu(menu, menuIcone);
-})
+const menu = document.querySelector('[data-tipo="menu"]');
+const menuIcone = document.querySelector('.bi-menu-abrir');
+const listaLinkMenu = document.querySelectorAll('[data-tipo="abreFechaMenu"]');
 
 listaLinkMenu.forEach(link => {
     link.addEventListener('click', () =>{
         abreFechaMenu(menu, menuIcone);
-    })
+    });
 })
